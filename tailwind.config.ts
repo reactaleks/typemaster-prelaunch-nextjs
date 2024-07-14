d2ee39185da298df15aa22848f484ba0cad12ddf
Aleks <a.ivanov.developer@gmail.com>
2024-07-14 10:00:43 +0100

    Project init, added fonts/colors/linespacing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,90 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'orange': '#F16718',
+      'bright-orange': '#FF9B62',
+      'dark-blue': '#162542',
+      'light-blue': '#7B8BAD',
+      'light-gray': '#E8EFF2',
+      'white': '#FFFFFF'
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        barlow: 'Barlow',
+        barlowm: 'Barlow Medium'
       },
+      lineHeight: {
+        headingxl: '56px',
+        headingm: '36px',
+        headings: '28px',
+        body: '26px',
+      },
+
+
+      gridTemplateColumns: {
+        '24': 'repeat(24, 1fr)'
+      },
+      gridTemplateRows: {
+        '24': 'repeat(24, 1fr)'
+      },
+      gridColumn: {
+        'span-13': 'span 13 / span 13',
+        'span-14': 'span 14 / span 14',
+        'span-15': 'span 15 / span 15',
+        'span-16': 'span 16 / span 16',
+        'span-17': 'span 17 / span 17',
+        'span-18': 'span 18 / span 18',
+        'span-19': 'span 19 / span 19',
+        'span-20': 'span 20 / span 20',
+        'span-21': 'span 21 / span 21',
+        'span-22': 'span 22 / span 22',
+        'span-23': 'span 23 / span 23',
+        'span-24': 'span 24 / span 24'
+      },
+      gridColStart: {
+        '13': '13',
+        '14': '14',
+        '15': '15',
+        '16': '16',
+        '17': '17',
+        '18': '18',
+        '19': '19',
+        '20': '20',
+        '21': '21',
+        '22': '22',
+        '23': '23',
+        '24': '24',
+      },
+      gridRow: {
+        'span-13': 'span 13 / span 13',
+        'span-14': 'span 14 / span 14',
+        'span-15': 'span 15 / span 15',
+        'span-16': 'span 16 / span 16',
+        'span-17': 'span 17 / span 17',
+        'span-18': 'span 18 / span 18',
+        'span-19': 'span 19 / span 19',
+        'span-20': 'span 20 / span 20',
+        'span-21': 'span 21 / span 21',
+        'span-22': 'span 22 / span 22',
+        'span-23': 'span 23 / span 23',
+        'span-24': 'span 24 / span 24',
+      },
+      
+      gridRowStart: {
+        '13': '13',
+        '14': '14',
+        '15': '15',
+        '16': '16',
+        '17': '17',
+        '18': '18',
+        '19': '19',
+        '20': '20',
+        '21': '21',
+        '22': '22',
+        '23': '23',
+        '24': '24',
+      }
     },
   },
   plugins: [],
